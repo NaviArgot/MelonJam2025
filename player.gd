ff3ea425dc3b44b97b159d8fd9a463db53319adb
Usu <244884326+NaviArgot@users.noreply.github.com>
2025-12-26 21:34:19 -0600

    Implemented animations

--- player.gd
+++ player.gd
@@ -1,26 +1,36 @@
 class_name Player
 extends CharacterBody3D
 
-enum DIR {UP, DOWN, LEFT, RIGHT}
+enum STATE {IDLE, WALKING, ATTACKING}
+enum TURNING {LEFT, RIGHT}
 
 @export var maxSpeed: float = 1.0
 
 var facing: Vector3 = Vector3(0.0, 0.0, -1.0)
 var speed: Vector3 = Vector3(0.0, 0.0, 0.0)
+var state: STATE = STATE.IDLE
+var sprites: Array[Sprite3D] = []
+var turningTween: Tween = null
 
 func receiveInput () -> void:
 	speed.x = 0.0
 	speed.z = 0.0
+	state = STATE.IDLE
 	if Input.is_action_pressed("up"):
 		speed.z = -maxSpeed
 	if Input.is_action_pressed("down"):
 		speed.z = +maxSpeed
 	if Input.is_action_pressed("right"):
 		speed.x = maxSpeed
+		$Animations.rotation.y = 0
 	if Input.is_action_pressed("left"):
 		speed.x = -maxSpeed
+		$Animations.rotation.y = PI
+	if speed.length_squared() > 0:
+		state = STATE.WALKING
 	if Input.is_action_pressed("attack"):
 		$Weapon.attack(facing)
+		state = STATE.ATTACKING
 	else:
 		$Weapon.disable()
 
@@ -34,6 +44,11 @@ func computeFacingTarget() -> void:
 	facing = facing.normalized()
 
 
+func enableAnim(animation: String) -> void:
+	if $AnimationPlayer.current_animation != animation:
+		$AnimationPlayer.play("RESET")
+	$AnimationPlayer.play(animation)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -41,6 +56,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	match state:
+		STATE.IDLE:
+			enableAnim("idle")
+		STATE.WALKING:
+			enableAnim("run")
+		STATE.ATTACKING:
+			enableAnim("run")
 	computeFacingTarget()
 	receiveInput()
 	move_and_collide(speed * delta)
